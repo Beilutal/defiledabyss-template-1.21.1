8db48bed568b0b4731e6151403d00fc0df8d0ea0
Beilutal <Baifengmost123@qq.com>
2025-06-27 12:34:46 +0800

    add something

---
+++ defiledabyss-template-1.21.1/src/main/java/com/beilutal/defiledabyss/datagen/DefiledAbyssDataGenerator.java
@@ -0,0 +1,17 @@
+package com.beilutal.defiledabyss.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+public class DefiledAbyssDataGenerator {
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        var output = generator.getPackOutput();
+        var lookupProvider = event.getLookupProvider();
+        var existingFileHelper = event.getExistingFileHelper();
+
+        // 在这里添加具体的数据生成器
+        // 例如，生成块标签、物品模型等
+        // generator.addProvider(true, new ModBlockTagProvider(output, lookupProvider, existingFileHelper));
+    }
+}
